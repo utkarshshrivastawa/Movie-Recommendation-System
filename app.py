d1bb37132d4f46ac3a02773624b6e37b32525112
Utkarsh Shrivastawa <92841647+utkarshshrivastawa@users.noreply.github.com>
2024-06-10 15:23:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import pickle
+import difflib
 import streamlit as st
 from streamlit_option_menu import option_menu
-import pandas as pd
-import difflib
 
-# Load the data
-movie_data = pd.read_csv('movies.csv')
+# Load the saved model and data
+movie_data = pickle.load(open("movie_data.sav", 'rb'))
+similarity = pickle.load(open("similarity.sav", 'rb'))
 
-# Load the saved model
-movie_model = pickle.load(open("movie_model.sav", 'rb'))
-
-# Sidebar menu
 with st.sidebar:
     selected = option_menu("Movie Recommendation Assistant",
                            ['Movie Recommendation System'],
@@ -19,28 +15,22 @@ with st.sidebar:
 
 # Movie Recommendation Page
 if selected == 'Movie Recommendation System':
-
     # Page title
     st.title('Movie Recommendation System Using ML')
-
-    # User input for movie name
-    movie_name = st.text_input('Enter your favourite movie name: ')
-
+    
+    movie_name = st.text_input('Enter your favourite movie name:')
+    
     if movie_name:
         list_of_titles = movie_data['title'].tolist()
         close_matches = difflib.get_close_matches(movie_name, list_of_titles)
-
+        
         if close_matches:
-            closest_match = close_matches[0]
-            index_movie = movie_data[movie_data.title == closest_match]['index'].values[0]
-
-            # Assuming similarity is a precomputed similarity matrix or use model to get similarity
-            similarity = movie_model  # this should be your similarity matrix or model
+            close_match = close_matches[0]
+            index_movie = movie_data[movie_data.title == close_match]['index'].values[0]
             similarity_score = list(enumerate(similarity[index_movie]))
             sorted_movies = sorted(similarity_score, key=lambda x: x[1], reverse=True)
-
-            # Display recommended movies
-            st.write('Movies suggested for you:\n')
+            
+            st.write('Movies suggested for you:')
             i = 1
             for movie in sorted_movies:
                 index = movie[0]
@@ -49,4 +39,4 @@ if selected == 'Movie Recommendation System':
                     st.write(f"{i}. {title_from_index}")
                     i += 1
         else:
-            st.write("No close matches found. Please try a different movie.")
+            st.write("No close matches found for the entered movie name.")
