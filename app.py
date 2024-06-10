fe1f597cfbfe9bdc0308d94081fd1f94af22150f
Utkarsh Shrivastawa <92841647+utkarshshrivastawa@users.noreply.github.com>
2024-06-10 15:07:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,24 +26,20 @@ if selected == 'Movie Recommendation System':
     # User input for movie name
     movie_name = st.text_input('Enter your favourite movie name: ')
     
-    if movie_name:
-        list_of_title = movie_data['title'].tolist()
-        close_match = difflib.get_close_matches(movie_name, list_of_title)
-        
-        if close_match:
-            close_matches = close_match[0]
-            index_movie = movie_data[movie_data.title == close_matches]['index'].values[0]
-            similarity_score = list(enumerate(movie_model[index_movie]))
-            sorted_movie = sorted(similarity_score, key=lambda x: x[1], reverse=True)
+    list_of_title = movie_data['title'].tolist()
+    close_match = difflib.get_close_matches(movie_name , list_of_title)
+    close_matches = close_match[0]
+    index_movie =movie_data[ movie_data.title == close_matches]['index'].values[0]
+    similarity_score = list(enumerate(similarity[index_movie]))
+    sorted_movie = sorted(similarity_score, key= lambda x:x[1], reverse =True)
             
-            # Display recommended movies
-            st.write('Movies suggested for you:\n')
-            i = 1
-            for movie in sorted_movie:
+    # Display recommended movies
+    st.write('Movies suggested for you:\n')
+    i = 1
+    for movie in sorted_movie:
                 index = movie[0]
                 title_from_index = movie_data[movie_data.index == index]['title'].values[0]
                 if i <= 10:
                     st.write(f"{i}. {title_from_index}")
                     i += 1
-        else:
-            st.write("No close matches found. Please check the movie name.")
+
